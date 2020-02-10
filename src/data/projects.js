7b25ac1d90f7f9a84bd2d91a3af1ff1728ed2dff
Rodolfo Roman <rromanv@gmail.com>
2020-02-09 21:06:59 -0600

    Portfolio finished

---
+++ src/data/projects.js
@@ -0,0 +1,45 @@
+import basictodo from '../assets/projects/01.png'
+import todob from '../assets/projects/02.png'
+import todot from '../assets/projects/03.png'
+import inventory from '../assets/projects/04.png'
+import vuetodo from '../assets/projects/05.png'
+
+const projects = [
+  {
+    name: 'Basic Todo App',
+    description: 'A Vanilla HTML, JS and CSS Todo Application',
+    image: basictodo,
+    hashtags: ['todo', 'html5', 'css', 'javascript'],
+    url: 'https://basictodo.rodolforoman.xyz/'
+  },
+  {
+    name: 'Todo App with Bootstrap',
+    description: 'A Todo Application in Vanilla JavasCript and Bootstrap',
+    image: todob,
+    hashtags: ['todo', 'html5', 'javascript', 'bootstrap'],
+    url: 'https://todob.rodolforoman.xyz/'
+  },
+  {
+    name: 'Todo App with Tailwind',
+    description: 'A Todo Application in Vanilla JavasCript and TailwindCSS',
+    image: todot,
+    hashtags: ['todo', 'html5', 'javascript', 'tailwind'],
+    url: 'https://todot.rodolforoman.xyz/'
+  },
+  {
+    name: 'Basic Inventory App',
+    description: 'A Basic Inventory App in Vue.js',
+    image: inventory,
+    hashtags: ['inventory', 'vuejs', 'javascript', 'tailwind'],
+    url: 'https://inventory.rodolforoman.xyz/'
+  },
+  {
+    name: 'Todo App with Tailwind and Vue.js',
+    description: 'A Todo Application in Vue.js and TailwindCSS',
+    image: vuetodo,
+    hashtags: ['todo', 'vuejs', 'javascript', 'tailwind'],
+    url: 'https://vuetodo.rodolforoman.xyz/'
+  }
+]
+
+export default projects
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,17 +1,37 @@
 <template>
   <div>
     <NavBar :ishome="false"></NavBar>
-    <div class="pt-24">hello there</div>
+    <div
+      class="pt-24 min-h-screen w-screen top flex flex-col flex-wrap justify-center items-center px-8 bg-gray-200 shadow-lg"
+    >
+      <div class="w-full flex flex-wrap mb-8">
+        <div
+          v-for="(project, index) in projects"
+          :key="index"
+          class="w-1/4 p-2"
+        >
+          <Card :project="project" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from '../components/NavBar'
+import Card from '../components/Card'
+import projects from '../data/projects'
 
 export default {
   name: 'Portfolio',
   components: {
-    NavBar
+    NavBar,
+    Card
+  },
+  data() {
+    return {
+      projects
+    }
   }
 }
 </script>
